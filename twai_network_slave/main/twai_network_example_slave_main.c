801d8fbb3065443836971438b8d4f9d8a26bf0e6
juliangarr <juliangarridoarana@gmail.com>
2025-05-08 13:34:22 +0100

    Posibles códigos simples de partida

--- twai_network_slave/main/twai_network_example_slave_main.c
+++ twai_network_slave/main/twai_network_example_slave_main.c
@@ -1,293 +1,97 @@
-/*
- * SPDX-FileCopyrightText: 2010-2024 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
-/*
- * The following example demonstrates a slave node in a TWAI network. The slave
- * node is responsible for sending data messages to the master. The example will
- * execute multiple iterations, with each iteration the slave node will do the
- * following:
- * 1) Start the TWAI driver
- * 2) Listen for ping messages from master, and send ping response
- * 3) Listen for start command from master
- * 4) Send data messages to master and listen for stop command
- * 5) Send stop response to master
- * 6) Stop the TWAI driver
- */
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
-#include "freertos/semphr.h"
-#include "esp_err.h"
-#include "esp_log.h"
+#include "driver/gpio.h"
 #include "driver/twai.h"
+#include "esp_log.h"
 
-/* --------------------- Definitions and static variables ------------------ */
-//Example Configuration
-#define DATA_PERIOD_MS                  50
-#define NO_OF_ITERS                     3
-#define ITER_DELAY_MS                   1000
-#define RX_TASK_PRIO                    8       //Receiving task priority
-#define TX_TASK_PRIO                    9       //Sending task priority
-#define CTRL_TSK_PRIO                   10      //Control task priority
-#define TX_GPIO_NUM                     CONFIG_EXAMPLE_TX_GPIO_NUM
-#define RX_GPIO_NUM                     CONFIG_EXAMPLE_RX_GPIO_NUM
-#define EXAMPLE_TAG                     "TWAI Slave"
+#define TAG "TWAI SLAVE"
 
-#define ID_MASTER_STOP_CMD              0x0A0
-#define ID_MASTER_START_CMD             0x0A1
-#define ID_MASTER_PING                  0x0A2
-#define ID_SLAVE_STOP_RESP              0x0B0
-#define ID_SLAVE_DATA                   0x0B1
-#define ID_SLAVE_PING_RESP              0x0B2
+#define BTN1_GPIO 4
+#define BTN2_GPIO 5
+#define TWAI_RX_GPIO CONFIG_EXAMPLE_RX_GPIO_NUM
+#define TWAI_TX_GPIO CONFIG_EXAMPLE_TX_GPIO_NUM
 
-typedef enum {
-    TX_SEND_PING_RESP,
-    TX_SEND_DATA,
-    TX_SEND_STOP_RESP,
-    TX_TASK_EXIT,
-} tx_task_action_t;
+#define ID_SLAVE_PING_RESP 0x0B2
+#define ID_SLAVE_DATA      0x0B1
+#define ID_MASTER_PING     0x0A2
 
-typedef enum {
-    RX_RECEIVE_PING,
-    RX_RECEIVE_START_CMD,
-    RX_RECEIVE_STOP_CMD,
-    RX_TASK_EXIT,
-} rx_task_action_t;
+volatile uint32_t pulse_count_btn1 = 0;
+volatile uint32_t pulse_count_btn2 = 0;
+volatile uint8_t btn1_state = 0;
+volatile uint8_t btn2_state = 0;
 
-static const twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(TX_GPIO_NUM, RX_GPIO_NUM, TWAI_MODE_NORMAL);
-static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_25KBITS();
-static const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+static void IRAM_ATTR btn1_isr_handler(void* arg) {
+    pulse_count_btn1++;
+    btn1_state = gpio_get_level(BTN1_GPIO);
+}
 
-static const twai_message_t ping_resp = {
-    // Message type and format settings
-    .extd = 0,              // Standard Format message (11-bit ID)
-    .rtr = 0,               // Send a data frame
-    .ss = 0,                // Not single shot
-    .self = 0,              // Not a self reception request
-    .dlc_non_comp = 0,      // DLC is less than 8
-    // Message ID and payload
-    .identifier = ID_SLAVE_PING_RESP,
-    .data_length_code = 0,
-    .data = {0},
-};
+static void IRAM_ATTR btn2_isr_handler(void* arg) {
+    pulse_count_btn2++;
+    btn2_state = gpio_get_level(BTN2_GPIO);
+}
 
-static const twai_message_t stop_resp = {
-    // Message type and format settings
-    .extd = 0,              // Standard Format message (11-bit ID)
-    .rtr = 0,               // Send a data frame
-    .ss = 0,                // Not single shot
-    .self = 0,              // Not a self reception request
-    .dlc_non_comp = 0,      // DLC is less than 8
-    // Message ID and payload
-    .identifier = ID_SLAVE_STOP_RESP,
-    .data_length_code = 0,
-    .data = {0},
-};
+void twai_slave_task(void *arg) {
+    // TWAI setup
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(TWAI_TX_GPIO, TWAI_RX_GPIO, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_25KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+    ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
+    ESP_ERROR_CHECK(twai_start());
 
-// Data bytes of data message will be initialized in the transmit task
-static twai_message_t data_message = {
-    // Message type and format settings
-    .extd = 0,              // Standard Format message (11-bit ID)
-    .rtr = 0,               // Send a data frame
-    .ss = 0,                // Not single shot
-    .self = 0,              // Not a self reception request
-    .dlc_non_comp = 0,      // DLC is less than 8
-    // Message ID and payload
-    .identifier = ID_SLAVE_DATA,
-    .data_length_code = 4,
-    .data = {1, 2, 3, 4},
-};
+    ESP_LOGI(TAG, "Esperando ping del maestro...");
+    while (true) {
+        twai_message_t rx_msg;
+        if (twai_receive(&rx_msg, pdMS_TO_TICKS(1000)) == ESP_OK) {
+            if (rx_msg.identifier == ID_MASTER_PING) {
+                ESP_LOGI(TAG, "Ping recibido, enviando respuesta...");
+                twai_message_t ping_resp = {
+                    .identifier = ID_SLAVE_PING_RESP,
+                    .data_length_code = 0,
+                    .extd = 0, .rtr = 0, .ss = 0
+                };
+                twai_transmit(&ping_resp, pdMS_TO_TICKS(100));
+                break;
+            }
+        }
+    }
 
-static QueueHandle_t tx_task_queue;
-static QueueHandle_t rx_task_queue;
-static SemaphoreHandle_t ctrl_task_sem;
-static SemaphoreHandle_t stop_data_sem;
-static SemaphoreHandle_t done_sem;
+    // Setup botones
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_POSEDGE,
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = (1ULL << BTN1_GPIO) | (1ULL << BTN2_GPIO),
+        .pull_down_en = 0,
+        .pull_up_en = 1,
+    };
+    gpio_config(&io_conf);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(BTN1_GPIO, btn1_isr_handler, NULL);
+    gpio_isr_handler_add(BTN2_GPIO, btn2_isr_handler, NULL);
 
-/* --------------------------- Tasks and Functions -------------------------- */
-
-static void twai_receive_task(void *arg)
-{
     while (1) {
-        rx_task_action_t action;
-        xQueueReceive(rx_task_queue, &action, portMAX_DELAY);
-        if (action == RX_RECEIVE_PING) {
-            //Listen for pings from master
-            twai_message_t rx_msg;
-            while (1) {
-                twai_receive(&rx_msg, portMAX_DELAY);
-                if (rx_msg.identifier == ID_MASTER_PING) {
-                    xSemaphoreGive(ctrl_task_sem);
-                    break;
-                }
-            }
-        } else if (action == RX_RECEIVE_START_CMD) {
-            //Listen for start command from master
-            twai_message_t rx_msg;
-            while (1) {
-                twai_receive(&rx_msg, portMAX_DELAY);
-                if (rx_msg.identifier == ID_MASTER_START_CMD) {
-                    xSemaphoreGive(ctrl_task_sem);
-                    break;
-                }
-            }
-        } else if (action == RX_RECEIVE_STOP_CMD) {
-            //Listen for stop command from master
-            twai_message_t rx_msg;
-            while (1) {
-                twai_receive(&rx_msg, portMAX_DELAY);
-                if (rx_msg.identifier == ID_MASTER_STOP_CMD) {
-                    xSemaphoreGive(stop_data_sem);
-                    xSemaphoreGive(ctrl_task_sem);
-                    break;
-                }
-            }
-        } else if (action == RX_TASK_EXIT) {
-            break;
-        }
-    }
-    vTaskDelete(NULL);
-}
+        uint8_t data[4];
+        data[0] = btn1_state;
+        data[1] = pulse_count_btn1;
+        data[2] = btn2_state;
+        data[3] = pulse_count_btn2;
 
-static void twai_transmit_task(void *arg)
-{
-    while (1) {
-        tx_task_action_t action;
-        xQueueReceive(tx_task_queue, &action, portMAX_DELAY);
+        twai_message_t msg = {
+            .identifier = ID_SLAVE_DATA,
+            .data_length_code = 4,
+            .data = { data[0], data[1], data[2], data[3] },
+            .extd = 0, .rtr = 0, .ss = 0
+        };
 
-        if (action == TX_SEND_PING_RESP) {
-            //Transmit ping response to master
-            twai_transmit(&ping_resp, portMAX_DELAY);
-            ESP_LOGI(EXAMPLE_TAG, "Transmitted ping response");
-            xSemaphoreGive(ctrl_task_sem);
-        } else if (action == TX_SEND_DATA) {
-            //Transmit data messages until stop command is received
-            ESP_LOGI(EXAMPLE_TAG, "Start transmitting data");
-            while (1) {
-                //FreeRTOS tick count used to simulate sensor data
-                uint32_t sensor_data = xTaskGetTickCount();
-                for (int i = 0; i < 4; i++) {
-                    data_message.data[i] = (sensor_data >> (i * 8)) & 0xFF;
-                }
-                twai_transmit(&data_message, portMAX_DELAY);
-                ESP_LOGI(EXAMPLE_TAG, "Transmitted data value %"PRIu32, sensor_data);
-                vTaskDelay(pdMS_TO_TICKS(DATA_PERIOD_MS));
-                if (xSemaphoreTake(stop_data_sem, 0) == pdTRUE) {
-                    break;
-                }
-            }
-        } else if (action == TX_SEND_STOP_RESP) {
-            //Transmit stop response to master
-            twai_transmit(&stop_resp, portMAX_DELAY);
-            ESP_LOGI(EXAMPLE_TAG, "Transmitted stop response");
-            xSemaphoreGive(ctrl_task_sem);
-        } else if (action == TX_TASK_EXIT) {
-            break;
-        }
-    }
-    vTaskDelete(NULL);
-}
-
-static void twai_control_task(void *arg)
-{
-    xSemaphoreTake(ctrl_task_sem, portMAX_DELAY);
-    tx_task_action_t tx_action;
-    rx_task_action_t rx_action;
-
-    for (int iter = 0; iter < NO_OF_ITERS; iter++) {
-        ESP_ERROR_CHECK(twai_start());
-        ESP_LOGI(EXAMPLE_TAG, "Driver started");
-
-        //Listen of pings from master
-        rx_action = RX_RECEIVE_PING;
-        xQueueSend(rx_task_queue, &rx_action, portMAX_DELAY);
-        xSemaphoreTake(ctrl_task_sem, portMAX_DELAY);
-
-        //Send ping response
-        tx_action = TX_SEND_PING_RESP;
-        xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
-        xSemaphoreTake(ctrl_task_sem, portMAX_DELAY);
-
-        //Listen for start command
-        rx_action = RX_RECEIVE_START_CMD;
-        xQueueSend(rx_task_queue, &rx_action, portMAX_DELAY);
-        xSemaphoreTake(ctrl_task_sem, portMAX_DELAY);
-
-        //Start sending data messages and listen for stop command
-        tx_action = TX_SEND_DATA;
-        rx_action = RX_RECEIVE_STOP_CMD;
-        xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
-        xQueueSend(rx_task_queue, &rx_action, portMAX_DELAY);
-        xSemaphoreTake(ctrl_task_sem, portMAX_DELAY);
-
-        //Send stop response
-        tx_action = TX_SEND_STOP_RESP;
-        xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
-        xSemaphoreTake(ctrl_task_sem, portMAX_DELAY);
-
-        //Wait for bus to become free
-        twai_status_info_t status_info;
-        twai_get_status_info(&status_info);
-        while (status_info.msgs_to_tx > 0) {
-            vTaskDelay(pdMS_TO_TICKS(100));
-            twai_get_status_info(&status_info);
-        }
-
-        ESP_ERROR_CHECK(twai_stop());
-        ESP_LOGI(EXAMPLE_TAG, "Driver stopped");
-        vTaskDelay(pdMS_TO_TICKS(ITER_DELAY_MS));
-    }
-
-    //Stop TX and RX tasks
-    tx_action = TX_TASK_EXIT;
-    rx_action = RX_TASK_EXIT;
-    xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
-    xQueueSend(rx_task_queue, &rx_action, portMAX_DELAY);
-
-    //Delete Control task
-    xSemaphoreGive(done_sem);
-    vTaskDelete(NULL);
-}
-
-void app_main(void)
-{
-    //Add short delay to allow master it to initialize first
-    for (int i = 3; i > 0; i--) {
-        printf("Slave starting in %d\n", i);
+        ESP_LOGI(TAG, "Enviando - BTN1: %d (%d Hz), BTN2: %d (%d Hz)", data[0], data[1], data[2], data[3]);
+        twai_transmit(&msg, pdMS_TO_TICKS(100));
+        pulse_count_btn1 = 0;
+        pulse_count_btn2 = 0;
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
 
-    //Create semaphores and tasks
-    tx_task_queue = xQueueCreate(1, sizeof(tx_task_action_t));
-    rx_task_queue = xQueueCreate(1, sizeof(rx_task_action_t));
-    ctrl_task_sem = xSemaphoreCreateBinary();
-    stop_data_sem  = xSemaphoreCreateBinary();
-    done_sem  = xSemaphoreCreateBinary();
-    xTaskCreatePinnedToCore(twai_receive_task, "TWAI_rx", 4096, NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
-    xTaskCreatePinnedToCore(twai_transmit_task, "TWAI_tx", 4096, NULL, TX_TASK_PRIO, NULL, tskNO_AFFINITY);
-    xTaskCreatePinnedToCore(twai_control_task, "TWAI_ctrl", 4096, NULL, CTRL_TSK_PRIO, NULL, tskNO_AFFINITY);
-
-    //Install TWAI driver, trigger tasks to start
-    ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
-    ESP_LOGI(EXAMPLE_TAG, "Driver installed");
-
-    xSemaphoreGive(ctrl_task_sem);              //Start Control task
-    xSemaphoreTake(done_sem, portMAX_DELAY);    //Wait for tasks to complete
-
-    //Uninstall TWAI driver
-    ESP_ERROR_CHECK(twai_driver_uninstall());
-    ESP_LOGI(EXAMPLE_TAG, "Driver uninstalled");
-
-    //Cleanup
-    vSemaphoreDelete(ctrl_task_sem);
-    vSemaphoreDelete(stop_data_sem);
-    vSemaphoreDelete(done_sem);
-    vQueueDelete(tx_task_queue);
-    vQueueDelete(rx_task_queue);
+void app_main(void) {
+    xTaskCreatePinnedToCore(twai_slave_task, "twai_slave_task", 4096, NULL, 10, NULL, tskNO_AFFINITY);
 }
